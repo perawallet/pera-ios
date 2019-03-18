f3280d8ffa2181a87dab942d4fb463bdad3fb192
goktugberkulu <goktug@hipolabs.com>
2019-03-18 20:42:54 +0300

    Implement basic welcome and introduction views

---
+++ Classes/ViewControllers/Authentication/Introduction/WelcomeViewController.swift
@@ -0,0 +1,47 @@
+//
+//  WelcomeViewController.swift
+//  algorand
+//
+//  Created by Göktuğ Berk Ulu on 15.03.2019.
+//  Copyright © 2019 hippo. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: BaseViewController {
+
+    // MARK: Components
+    
+    private lazy var welcomeView: WelcomeView = {
+        let view = WelcomeView()
+        return view
+    }()
+    
+    // MARK: Setup
+    
+    override func configureAppearance() {
+        view.backgroundColor = rgb(0.95, 0.96, 0.96)
+    }
+    
+    override func prepareLayout() {
+        view.addSubview(welcomeView)
+        
+        welcomeView.snp.makeConstraints { make in
+            make.leading.trailing.top.equalToSuperview()
+            make.bottom.safeEqualToBottom(of: self)
+        }
+    }
+    
+    override func linkInteractors() {
+        welcomeView.delegate = self
+    }
+}
+
+// MARK: WelcomeViewDelegate
+
+extension WelcomeViewController: WelcomeViewDelegate {
+    
+    func welcomeViewDidTapDoneButton(_ introductionView: WelcomeView) {
+        open(.localAuthenticationPreference, by: .push)
+    }
+}
---
+++ Classes/Views/Authentication/Introduction/IntroductionView.swift
@@ -0,0 +1,117 @@
+//
+//  IntroductionView.swift
+//  algorand
+//
+//  Created by Göktuğ Berk Ulu on 15.03.2019.
+//  Copyright © 2019 hippo. All rights reserved.
+//
+
+import UIKit
+
+protocol IntroductionViewDelegate: class {
+    
+    func introductionViewDidTapCreateAccountButton(_ introductionView: IntroductionView)
+    func introductionViewDidTapRecoverButton(_ introductionView: IntroductionView)
+}
+
+class IntroductionView: BaseView {
+    
+    private struct LayoutConstants: AdaptiveLayoutConstants {
+        let verticalInset: CGFloat = 94.0
+        let createButtonTopInset: CGFloat = 42.0
+        let bottomInset: CGFloat = 83.0
+        let buttonMinimumTopInset: CGFloat = 10.0
+    }
+    
+    private let layout = Layout<LayoutConstants>()
+    
+    // MARK: Components
+    
+    private lazy var logoImageView = UIImageView(image: img("logo-small"))
+    
+    private lazy var detailImageView = UIImageView(image: img("image-registration"))
+    
+    private lazy var createAccountButton: MainButton = {
+        let button = MainButton(title: "introduction-create-title".localized)
+        return button
+    }()
+    
+    private lazy var recoverButton: UIButton = {
+        UIButton(type: .custom)
+            .withTitleColor(rgba(0.04, 0.05, 0.07, 0.57))
+            .withTitle("introduction-recover-title".localized)
+            .withAlignment(.center)
+            .withFont(UIFont.systemFont(ofSize: 14.0, weight: .semibold))
+    }()
+    
+    weak var delegate: IntroductionViewDelegate?
+    
+    // MARK: Configuration
+    
+    override func configureAppearance() {
+        backgroundColor = rgb(0.95, 0.96, 0.96)
+    }
+    
+    override func setListeners() {
+        createAccountButton.addTarget(self, action: #selector(notifyDelegateToCreateAccountButtonTapped), for: .touchUpInside)
+        recoverButton.addTarget(self, action: #selector(notifyDelegateToRecoverButtonTapped), for: .touchUpInside)
+    }
+    
+    // MARK: Layout
+    
+    override func prepareLayout() {
+        setupLogoImageViewLayout()
+        setupDetailImageViewLayout()
+        setupCreateAccountButtonLayout()
+        setupRecoverButtonLayout()
+    }
+    
+    private func setupLogoImageViewLayout() {
+        addSubview(logoImageView)
+        
+        logoImageView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview().inset(layout.current.verticalInset)
+        }
+    }
+    
+    private func setupDetailImageViewLayout() {
+        addSubview(detailImageView)
+        
+        detailImageView.snp.makeConstraints { make in
+            make.top.lessThanOrEqualTo(logoImageView.snp.bottom).offset(layout.current.verticalInset)
+            make.centerX.equalToSuperview()
+        }
+    }
+    
+    private func setupCreateAccountButtonLayout() {
+        addSubview(createAccountButton)
+        
+        createAccountButton.snp.makeConstraints { make in
+            make.top.equalTo(detailImageView.snp.bottom).offset(layout.current.createButtonTopInset)
+            make.centerX.equalToSuperview()
+        }
+    }
+    
+    private func setupRecoverButtonLayout() {
+        addSubview(recoverButton)
+        
+        recoverButton.snp.makeConstraints { make in
+            make.top.greaterThanOrEqualTo(createAccountButton.snp.bottom).offset(layout.current.buttonMinimumTopInset)
+            make.bottom.equalToSuperview().inset(layout.current.bottomInset)
+            make.centerX.equalToSuperview()
+        }
+    }
+    
+    // MARK: Actions
+    
+    @objc
+    func notifyDelegateToCreateAccountButtonTapped() {
+        delegate?.introductionViewDidTapCreateAccountButton(self)
+    }
+
+    @objc
+    func notifyDelegateToRecoverButtonTapped() {
+        delegate?.introductionViewDidTapRecoverButton(self)
+    }
+}
