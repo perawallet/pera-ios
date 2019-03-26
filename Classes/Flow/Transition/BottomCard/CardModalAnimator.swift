4493db1b0f9552cf2febdf8127e36056f25d0c17
goktugberkulu <goktug@hipolabs.com>
2019-03-26 15:59:27 +0300

    Add card modal presentation

---
+++ Classes/Flow/Transition/BottomCard/CardModalAnimator.swift
@@ -0,0 +1,58 @@
+//
+//  CardModalAnimator.swift
+//  algorand
+//
+//  Created by Göktuğ Berk Ulu on 26.03.2019.
+//  Copyright © 2019 hippo. All rights reserved.
+//
+
+import UIKit
+
+class CardModalAnimator: AnimatorObjectType {
+    
+    var config: Configuration
+    
+    required init(config: Configuration) {
+        self.config = config
+        
+        super.init()
+    }
+    
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        switch config.animationMode {
+        case .normal(let duration), .spring(let duration, _, _):
+            return duration
+        }
+    }
+    
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        let containerView = transitionContext.containerView
+        
+        if transitionContext.isPresenting {
+            if let toView = transitionContext.toView {
+                containerView.addSubview(toView)
+            }
+        }
+        
+        let animatingView = transitionContext.animatedView
+        let animatingViewController = transitionContext.animatedPresentableViewController
+        
+        let finalFrame = transitionContext.finalFrame
+        
+        var initialFrame = finalFrame
+        initialFrame.origin.y = containerView.frame.height + finalFrame.height
+        
+        config.animationMode.animate({
+            animatingView?.frame = transitionContext.isPresenting ? finalFrame : initialFrame
+            animatingViewController?.alongsideAnimatedTransition()
+        },
+        before: {
+            animatingView?.frame = transitionContext.isPresenting ? initialFrame : finalFrame
+            animatingViewController?.beforeAnimatedTransition()
+        },
+        after: { _ in
+            animatingViewController?.afterAnimatedTransition()
+            transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
+        })
+    }
+}
