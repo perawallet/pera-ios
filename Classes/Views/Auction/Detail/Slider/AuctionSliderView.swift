bbf3650c4e7cbc3cc36b9c23108aa660d83f2922
goktugberkulu <goktug@hipolabs.com>
2019-05-17 12:38:10 +0300

    Reorder file grouping

---
+++ Classes/Views/Auction/Detail/Slider/AuctionSliderView.swift
@@ -0,0 +1,13 @@
+//
+//  AuctionSliderView.swift
+//  algorand
+//
+//  Created by Göktuğ Berk Ulu on 17.05.2019.
+//  Copyright © 2019 hippo. All rights reserved.
+//
+
+import UIKit
+
+class AuctionSliderView: BaseView {
+
+}
