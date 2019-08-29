2aa1bd0fa25541ab31bf4fcba19f15810ea0fc7f
goktugberkulu <goktug@hipolabs.com>
2019-08-29 22:10:25 +0300

    Add feedback requests
    Added mobile api to new environment
    Added feedback categories and feedback post endpoints
    Added models and draft for feedback

---
+++ Classes/Networking/Drafts/FeedbackDraft.swift
@@ -0,0 +1,15 @@
+//
+//  FeedbackDraft.swift
+//  algorand
+//
+//  Created by Göktuğ Berk Ulu on 29.08.2019.
+//  Copyright © 2019 hippo. All rights reserved.
+//
+
+import Foundation
+
+struct FeedbackDraft {
+    let note: String
+    let category: String
+    let email: String?
+}
