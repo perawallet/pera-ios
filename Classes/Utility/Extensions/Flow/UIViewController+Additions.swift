7bbc24e9e7072e52be74e335e9816db8f74b681b
goktugberkulu <goktug@hipolabs.com>
2019-12-20 01:44:20 +0300

    Change top view controller structure

--- Classes/Utility/Extensions/Flow/UIViewController+Additions.swift
+++ Classes/Utility/Extensions/Flow/UIViewController+Additions.swift
@@ -9,6 +9,21 @@
 import UIKit
 
 extension UIViewController {
+    var topMostController: UIViewController? {
+        if let controller = self as? UINavigationController {
+            return controller.topViewController?.topMostController
+        }
+        if let controller = self as? UISplitViewController {
+            return controller.viewControllers.last?.topMostController
+        }
+        if let controller = self as? UITabBarController {
+            return controller.selectedViewController?.topMostController
+        }
+        if let controller = presentedViewController {
+            return controller.topMostController
+        }
+        return self
+    }
     
     func displaySimpleAlertWith(title: String, message: String, handler: ((UIAlertAction) -> Void)? = nil) {
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
