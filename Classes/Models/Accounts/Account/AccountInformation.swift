c9333cf427b66ffeb8b5938c3484b798786736ea
goktugberkulu <goktug@hipolabs.com>
2020-02-13 01:19:30 +0300

    Add account types
    Added account types for ledger, watcher, standard and multi sig accounts

--- Classes/Models/Accounts/Account/AccountInformation.swift
+++ Classes/Models/Accounts/Account/AccountInformation.swift
@@ -11,10 +11,19 @@ import Magpie
 class AccountInformation: Model {
     let address: String
     var name: String
+    var type: AccountType = .standard
     
-    init(address: String, name: String) {
+    init(address: String, name: String, type: AccountType = .standard) {
         self.address = address
         self.name = name
+        self.type = type
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        address = try container.decode(String.self, forKey: .address)
+        name = try container.decode(String.self, forKey: .name)
+        type = try container.decodeIfPresent(AccountType.self, forKey: .type) ?? .standard
     }
 }
 
@@ -23,8 +32,15 @@ extension AccountInformation {
         self.name = name
     }
     
-    func mnemonics() -> [String] {
-        return UIApplication.shared.appConfiguration?.session.mnemonics(forAccount: address) ?? []
+    func mnemonics() -> [String]? {
+        if type == .watcher {
+            return nil
+        }
+        return UIApplication.shared.appConfiguration?.session.mnemonics(forAccount: address)
+    }
+    
+    func hasWriteAccess() -> Bool {
+        return type != .watcher
     }
     
     func encoded() -> Data? {
@@ -36,6 +52,7 @@ extension AccountInformation {
     enum CodingKeys: String, CodingKey {
         case address = "address"
         case name = "name"
+        case type = "type"
     }
 }
 
@@ -46,3 +63,12 @@ extension AccountInformation: Equatable {
         return lhs.address == rhs.address
     }
 }
+
+enum AccountType: String, Model {
+    case standard = "standard"
+    case watcher = "watcher"
+    case ledger = "ledger"
+    case multiSig = "multiSig"
+}
+
+extension AccountType: Encodable { }
