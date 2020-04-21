47a0def4500e5b7d880eae5d9ab1718ecef785eb
goktugberkulu <goktugberkulu@users.noreply.github.com>
2020-04-21 21:23:24 +0300

    :lipstick: Implement add account screen

--- Classes/ViewControllers/Authentication/Introduction/AddNewAccountViewController.swift
+++ Classes/ViewControllers/Authentication/Introduction/AddNewAccountViewController.swift
@@ -10,4 +10,43 @@ import UIKit
 
 class AddNewAccountViewController: BaseViewController {
     
+    private lazy var addNewAccountView = AddNewAccountView()
+        
+    override func configureAppearance() {
+        super.configureAppearance()
+        view.backgroundColor = color("secondaryBackground")
+    }
+        
+    override func prepareLayout() {
+        super.prepareLayout()
+        setupAddNewAccountViewLayout()
+    }
+        
+    override func linkInteractors() {
+        addNewAccountView.delegate = self
+    }
+}
+
+extension AddNewAccountViewController {
+    private func setupAddNewAccountViewLayout() {
+        view.addSubview(addNewAccountView)
+        
+        addNewAccountView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+}
+
+extension AddNewAccountViewController: AddNewAccountViewDelegate {
+    func addNewAccountViewDidTapCreateAccountButton(_ addNewAccountView: AddNewAccountView) {
+        open(.passphraseView(address: "temp"), by: .push)
+    }
+        
+    func addNewAccountViewDidTapPairLedgerAccountButton(_ addNewAccountView: AddNewAccountView) {
+        open(.ledgerTutorial(mode: .new), by: .push)
+    }
+        
+    func addNewAccountViewDidTapRecoverButton(_ addNewAccountView: AddNewAccountView) {
+        open(.accountRecover(mode: .new), by: .push)
+    }
 }
