85bbfe08701acb53208cf0072c1330bad9a7163a
goktugberkulu <goktugberkulu@users.noreply.github.com>
2020-05-13 17:35:03 +0300

    :sparkles: Implement multiple shadows

--- Classes/Styling/Core/Shadow/Shadow.swift
+++ Classes/Styling/Core/Shadow/Shadow.swift
@@ -14,3 +14,9 @@ struct Shadow {
     let radius: CGFloat
     let opacity: Float
 }
+
+let smallTopShadow = Shadow(color: SharedColors.smallTopShadow, offset: CGSize(width: 0.0, height: 4.0), radius: 6.0, opacity: 1.0)
+let smallBottomShadow = Shadow(color: SharedColors.smallBottomShadow, offset: CGSize(width: 0.0, height: 1.0), radius: 3.0, opacity: 1.0)
+let mediumTopShadow = Shadow(color: SharedColors.mediumTopShadow, offset: CGSize(width: 0.0, height: 4.0), radius: 12.0, opacity: 1.0)
+let mediumBottomShadow = Shadow(color: SharedColors.mediumBottomShadow, offset: CGSize(width: 0.0, height: 2.0), radius: 6.0, opacity: 1.0)
+let errorShadow = Shadow(color: SharedColors.errorShadow, offset: CGSize(width: 0.0, height: 8.0), radius: 20.0, opacity: 1.0)
