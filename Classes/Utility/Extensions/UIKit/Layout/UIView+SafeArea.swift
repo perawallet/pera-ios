294d0553f298d01625f33b761fa3352255e6d29d
goktugberkulu <goktugberkulu@users.noreply.github.com>
2020-05-25 16:30:20 +0300

    :construction: Add additional helper extensions from Macaroon

--- Classes/Utility/Extensions/UIKit/Layout/UIView+SafeArea.swift
+++ Classes/Utility/Extensions/UIKit/Layout/UIView+SafeArea.swift
@@ -9,12 +9,18 @@
 import UIKit
 
 extension UIView {
-    
     var safeAreaBottom: CGFloat {
         return UIApplication.shared.safeAreaBottom
     }
     
     var safeAreaTop: CGFloat {
         return UIApplication.shared.safeAreaTop
+    }
+    
+    var compactSafeAreaInsets: UIEdgeInsets {
+        if let window = window {
+            return window.safeAreaInsets
+        }
+        return UIApplication.shared.keyWindow?.safeAreaInsets ?? .zero
     }
 }
