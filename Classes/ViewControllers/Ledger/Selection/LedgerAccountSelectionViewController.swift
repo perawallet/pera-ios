e4d3d7d838b2b427ef115037e69cd6e09f43e6c6
goktugberkulu <goktugberkulu@users.noreply.github.com>
2020-08-25 03:27:57 +0300

    :sparkles: Implement ledger account selection screen
    Implemented screen actions for ledger account selection

--- Classes/ViewControllers/Ledger/Selection/LedgerAccountSelectionViewController.swift
+++ Classes/ViewControllers/Ledger/Selection/LedgerAccountSelectionViewController.swift
@@ -7,21 +7,57 @@
 //
 
 import UIKit
+import SVProgressHUD
 
 class LedgerAccountSelectionViewController: BaseViewController {
     
     private lazy var ledgerAccountSelectionView = LedgerAccountSelectionView()
     
-    private let ledger: LedgerDetail
+    private lazy var accountManager: AccountManager? = {
+        guard let api = self.api else {
+            return nil
+        }
+        let manager = AccountManager(api: api)
+        return manager
+    }()
     
-    init(ledger: LedgerDetail, configuration: ViewControllerConfiguration) {
+    private let ledger: LedgerDetail
+    private let mode: AccountSetupMode
+
+    private lazy var dataSource: LedgerAccountSelectionDataSource = {
+        guard let api = api else {
+            fatalError("API should be set.")
+        }
+        return LedgerAccountSelectionDataSource(api: api, ledger: ledger)
+    }()
+    
+    private lazy var listLayout = LedgerAccountSelectionListLayout(dataSource: dataSource)
+    
+    init(mode: AccountSetupMode, ledger: LedgerDetail, configuration: ViewControllerConfiguration) {
+        self.mode = mode
         self.ledger = ledger
         super.init(configuration: configuration)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        ledgerAccountSelectionView.setLoadingState()
+        
+        SVProgressHUD.show(withStatus: "title-loading".localized)
+        dataSource.loadData()
     }
     
     override func configureAppearance() {
         super.configureAppearance()
         title = ledger.name
+    }
+    
+    override func linkInteractors() {
+        ledgerAccountSelectionView.delegate = self
+        ledgerAccountSelectionView.setDataSource(dataSource)
+        ledgerAccountSelectionView.setListDelegate(listLayout)
+        dataSource.delegate = self
+        listLayout.delegate = self
     }
     
     override func prepareLayout() {
@@ -39,8 +75,73 @@ extension LedgerAccountSelectionViewController {
     }
 }
 
+extension LedgerAccountSelectionViewController: LedgerAccountSelectionDataSourceDelegate {
+    func ledgerAccountSelectionDataSource(
+        _ ledgerAccountSelectionDataSource: LedgerAccountSelectionDataSource,
+        didFetch accounts: [Account]
+    ) {
+        SVProgressHUD.showSuccess(withStatus: "title-done".localized)
+        SVProgressHUD.dismiss()
+        
+        ledgerAccountSelectionView.setNormalState()
+        ledgerAccountSelectionView.reloadData()
+    }
+    
+    func ledgerAccountSelectionDataSourceDidFailToFetch(_ ledgerAccountSelectionDataSource: LedgerAccountSelectionDataSource) {
+        SVProgressHUD.showError(withStatus: nil)
+        SVProgressHUD.dismiss()
+        
+        ledgerAccountSelectionView.setErrorState()
+        ledgerAccountSelectionView.reloadData()
+    }
+    
+    func ledgerAccountSelectionDataSourceDidCopyAuthAddress(_ ledgerAccountSelectionDataSource: LedgerAccountSelectionDataSource) {
+        displaySimpleAlertWith(title: "qr-creation-copied".localized, message: "")
+        UIPasteboard.general.string = dataSource.account(at: 0)?.authAddress
+    }
+}
+
+extension LedgerAccountSelectionViewController: LedgerAccountSelectionListLayoutDelegate {
+    func ledgerAccountSelectionListLayout(_ listLayout: LedgerAccountSelectionListLayout, didSelectAccountAt indexPath: IndexPath) {
+        setAddButtonEnabledIfNeeded()
+    }
+    
+    func ledgerAccountSelectionListLayout(_ listLayout: LedgerAccountSelectionListLayout, didDeselectAccountAt indexPath: IndexPath) {
+        setAddButtonEnabledIfNeeded()
+    }
+}
+
 extension LedgerAccountSelectionViewController: LedgerAccountSelectionViewDelegate {
     func ledgerAccountSelectionViewDidAddAccount(_ ledgerAccountSelectionView: LedgerAccountSelectionView) {
+        if ledgerAccountSelectionView.selectedIndexes.isEmpty {
+            return
+        }
         
+        RegistrationEvent(type: .ledger).logEvent()
+        dataSource.saveSelectedAccounts(ledgerAccountSelectionView.selectedIndexes)
+        launchHome()
+    }
+    
+    private func launchHome() {
+        SVProgressHUD.show(withStatus: "title-loading".localized)
+        accountManager?.fetchAllAccounts(isVerifiedAssetsIncluded: true) {
+            SVProgressHUD.showSuccess(withStatus: "title-done".localized)
+            SVProgressHUD.dismiss(withDelay: 1.0) {
+                switch self.mode {
+                case .initialize:
+                    self.dismiss(animated: false) {
+                        UIApplication.shared.rootViewController()?.setupTabBarController()
+                    }
+                case .new:
+                    self.closeScreen(by: .dismiss, animated: false)
+                case .rekey:
+                    break
+                }
+            }
+        }
+    }
+    
+    private func setAddButtonEnabledIfNeeded() {
+        ledgerAccountSelectionView.setEnabled(!ledgerAccountSelectionView.selectedIndexes.isEmpty)
     }
 }
