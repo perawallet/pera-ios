116e88b971d522f60622537fb517218919cb41dc
goktugberkulu <goktugberkulu@users.noreply.github.com>
2020-09-21 19:14:13 +0300

    :sparkles: Implement watch account addition screen

---
+++ Classes/ViewControllers/Authentication/WatchAccount/WatchAccountAdditionViewController.swift
@@ -0,0 +1,209 @@
+//
+//  WatchAccountAdditionViewController.swift
+//  algorand
+//
+//  Created by Göktuğ Berk Ulu on 20.09.2020.
+//  Copyright © 2020 hippo. All rights reserved.
+//
+
+import UIKit
+import SVProgressHUD
+
+class WatchAccountAdditionViewController: BaseScrollViewController {
+    
+    private lazy var bottomModalPresenter = CardModalPresenter(
+        config: ModalConfiguration(
+            animationMode: .normal(duration: 0.25),
+            dismissMode: .none
+        ),
+        initialModalSize: .custom(CGSize(width: view.frame.width, height: 338.0))
+    )
+    
+    private lazy var watchAccountAdditionView = WatchAccountAdditionView()
+    
+    private var keyboardController = KeyboardController()
+    
+    private lazy var accountManager: AccountManager? = {
+        guard let api = self.api else {
+            return nil
+        }
+        let manager = AccountManager(api: api)
+        return manager
+    }()
+    
+    private let accountSetupFlow: AccountSetupFlow
+    
+    init(accountSetupFlow: AccountSetupFlow, configuration: ViewControllerConfiguration) {
+        self.accountSetupFlow = accountSetupFlow
+        super.init(configuration: configuration)
+    }
+    
+    override func configureAppearance() {
+        super.configureAppearance()
+        title = "title-watch-account".localized
+    }
+    
+    override func setListeners() {
+        super.setListeners()
+        keyboardController.beginTracking()
+    }
+    
+    override func linkInteractors() {
+        keyboardController.dataSource = self
+        watchAccountAdditionView.delegate = self
+        scrollView.touchDetectingDelegate = self
+    }
+    
+    override func prepareLayout() {
+        super.prepareLayout()
+        setupWatchAccountAdditionViewLayout()
+    }
+}
+
+extension WatchAccountAdditionViewController {
+    private func setupWatchAccountAdditionViewLayout() {
+        contentView.addSubview(watchAccountAdditionView)
+        
+        watchAccountAdditionView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+}
+
+extension WatchAccountAdditionViewController: WatchAccountAdditionViewDelegate {
+    func watchAccountAdditionViewDidScanQR(_ watchAccountAdditionView: WatchAccountAdditionView) {
+        if !UIImagePickerController.isSourceTypeAvailable(.camera) {
+            displaySimpleAlertWith(title: "qr-scan-error-title".localized, message: "qr-scan-error-message".localized)
+            return
+        }
+        
+        guard let qrScannerViewController = open(.qrScanner, by: .push) as? QRScannerViewController else {
+            return
+        }
+        
+        qrScannerViewController.delegate = self
+    }
+    
+    func watchAccountAdditionViewDidAddAccount(_ watchAccountAdditionView: WatchAccountAdditionView) {
+        guard let address = watchAccountAdditionView.addressInputView.inputTextView.text,
+            !address.isEmpty,
+            address.isValidatedAddress() else {
+            displaySimpleAlertWith(title: "title-error".localized, message: "watch-account-error-address".localized)
+            return
+        }
+        
+        if session?.account(from: address) != nil {
+            displaySimpleAlertWith(title: "title-error".localized, message: "recover-from-seed-verify-exist-error".localized)
+            return
+        }
+        
+        let account = AccountInformation(address: address, name: address.shortAddressDisplay(), type: .watch)
+        let user: User
+        
+        if let authenticatedUser = session?.authenticatedUser {
+            user = authenticatedUser
+            if session?.authenticatedUser?.account(address: address) != nil {
+                user.updateAccount(account)
+            } else {
+                user.addAccount(account)
+            }
+        } else {
+            user = User(accounts: [account])
+        }
+        
+        session?.addAccount(Account(accountInformation: account))
+        session?.authenticatedUser = user
+        
+        view.endEditing(true)
+        
+        RegistrationEvent(type: .watch).logEvent()
+        
+        let configurator = BottomInformationBundle(
+            title: "recover-from-seed-verify-pop-up-title".localized,
+            image: img("img-green-checkmark"),
+            explanation: "recover-from-seed-verify-pop-up-explanation".localized,
+            actionTitle: "title-go-home".localized,
+            actionImage: img("bg-main-button")) {
+                self.launchHome(with: account)
+        }
+        
+        open(
+            .bottomInformation(mode: .confirmation, configurator: configurator),
+            by: .customPresentWithoutNavigationController(
+                presentationStyle: .custom,
+                transitionStyle: nil,
+                transitioningDelegate: bottomModalPresenter
+            )
+        )
+    }
+    
+    private func launchHome(with account: AccountInformation) {
+        SVProgressHUD.show(withStatus: "title-loading".localized)
+        accountManager?.fetchAllAccounts(isVerifiedAssetsIncluded: true) {
+            SVProgressHUD.showSuccess(withStatus: "title-done".localized)
+            SVProgressHUD.dismiss(withDelay: 1.0) {
+                switch self.accountSetupFlow {
+                case .initializeAccount:
+                    DispatchQueue.main.async {
+                        self.dismiss(animated: false) {
+                            UIApplication.shared.rootViewController()?.setupTabBarController()
+                        }
+                    }
+                case .addNewAccount:
+                    self.closeScreen(by: .dismiss, animated: false)
+                }
+            }
+        }
+    }
+}
+
+extension WatchAccountAdditionViewController: QRScannerViewControllerDelegate {
+    func qrScannerViewController(_ controller: QRScannerViewController, didRead qrText: QRText, completionHandler: EmptyHandler?) {
+        guard qrText.mode == .mnemonic else {
+            displaySimpleAlertWith(title: "title-error".localized, message: "qr-scan-should-scan-mnemonics-message".localized) { _ in
+                if let handler = completionHandler {
+                    handler()
+                }
+            }
+            return
+        }
+        
+        watchAccountAdditionView.addressInputView.value = qrText.qrText()
+    }
+    
+    func qrScannerViewController(_ controller: QRScannerViewController, didFail error: QRScannerError, completionHandler: EmptyHandler?) {
+        displaySimpleAlertWith(title: "title-error".localized, message: "qr-scan-should-scan-valid-qr".localized) { _ in
+            if let handler = completionHandler {
+                handler()
+            }
+        }
+    }
+}
+
+extension WatchAccountAdditionViewController: KeyboardControllerDataSource {
+    func bottomInsetWhenKeyboardPresented(for keyboardController: KeyboardController) -> CGFloat {
+        return 15.0
+    }
+    
+    func firstResponder(for keyboardController: KeyboardController) -> UIView? {
+        return watchAccountAdditionView.addressInputView
+    }
+    
+    func containerView(for keyboardController: KeyboardController) -> UIView {
+        return contentView
+    }
+    
+    func bottomInsetWhenKeyboardDismissed(for keyboardController: KeyboardController) -> CGFloat {
+        return 0.0
+    }
+}
+
+extension WatchAccountAdditionViewController: TouchDetectingScrollViewDelegate {
+    func scrollViewDidDetectTouchEvent(scrollView: TouchDetectingScrollView, in point: CGPoint) {
+        if watchAccountAdditionView.nextButton.frame.contains(point) ||
+            watchAccountAdditionView.addressInputView.frame.contains(point) {
+            return
+        }
+        contentView.endEditing(true)
+    }
+}
---
+++ Classes/Views/Authentication/WatchAccount/WatchAccountAdditionView.swift
@@ -0,0 +1,103 @@
+//
+//  WatchAccountAdditionView.swift
+//  algorand
+//
+//  Created by Göktuğ Berk Ulu on 20.09.2020.
+//  Copyright © 2020 hippo. All rights reserved.
+//
+
+import UIKit
+
+class WatchAccountAdditionView: BaseView {
+    
+    private let layout = Layout<LayoutConstants>()
+    
+    weak var delegate: WatchAccountAdditionViewDelegate?
+    
+    private(set) lazy var addressInputView: MultiLineInputField = {
+        let addressInputView = MultiLineInputField(displaysRightInputAccessoryButton: true)
+        addressInputView.explanationLabel.text = "watch-account-input-explanation".localized
+        addressInputView.placeholderLabel.text = "watch-account-input-placeholder".localized
+        addressInputView.nextButtonMode = .submit
+        addressInputView.inputTextView.autocorrectionType = .no
+        addressInputView.inputTextView.autocapitalizationType = .none
+        addressInputView.rightInputAccessoryButton.setImage(img("icon-qr-scan"), for: .normal)
+        addressInputView.inputTextView.textContainer.heightTracksTextView = true
+        addressInputView.inputTextView.isScrollEnabled = false
+        return addressInputView
+    }()
+    
+    private(set) lazy var nextButton = MainButton(title: "title-next".localized)
+    
+    override func linkInteractors() {
+        addressInputView.delegate = self
+    }
+    
+    override func setListeners() {
+        nextButton.addTarget(self, action: #selector(notifyDelegateToOpenNextScreen), for: .touchUpInside)
+    }
+    
+    override func prepareLayout() {
+        setupAddressInputViewLayout()
+        setupNextButtonLayout()
+    }
+}
+
+extension WatchAccountAdditionView {
+    @objc
+    func notifyDelegateToOpenNextScreen() {
+        delegate?.watchAccountAdditionViewDidAddAccount(self)
+    }
+}
+
+extension WatchAccountAdditionView {
+    private func setupAddressInputViewLayout() {
+        addSubview(addressInputView)
+        
+        addressInputView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview()
+            make.top.equalToSuperview().inset(layout.current.topInset)
+        }
+    }
+
+    private func setupNextButtonLayout() {
+        addSubview(nextButton)
+        
+        nextButton.snp.makeConstraints { make in
+            make.top.equalTo(addressInputView.snp.bottom).offset(layout.current.buttonTopInset)
+            make.bottom.lessThanOrEqualToSuperview().inset(layout.current.buttonBottomInset)
+            make.leading.trailing.equalToSuperview().inset(layout.current.horizontalInset)
+            make.centerX.equalToSuperview()
+        }
+    }
+}
+
+extension WatchAccountAdditionView {
+    func beginEditing() {
+        addressInputView.beginEditing()
+    }
+}
+
+extension WatchAccountAdditionView: InputViewDelegate {
+    func inputViewDidTapAccessoryButton(inputView: BaseInputView) {
+        delegate?.watchAccountAdditionViewDidScanQR(self)
+    }
+    
+    func inputViewDidReturn(inputView: BaseInputView) {
+        delegate?.watchAccountAdditionViewDidAddAccount(self)
+    }
+}
+
+extension WatchAccountAdditionView {
+    private struct LayoutConstants: AdaptiveLayoutConstants {
+        let topInset: CGFloat = 36.0
+        let buttonBottomInset: CGFloat = 15.0
+        let buttonTopInset: CGFloat = 24.0
+        let horizontalInset: CGFloat = 20.0
+    }
+}
+
+protocol WatchAccountAdditionViewDelegate: class {
+    func watchAccountAdditionViewDidScanQR(_ watchAccountAdditionView: WatchAccountAdditionView)
+    func watchAccountAdditionViewDidAddAccount(_ watchAccountAdditionView: WatchAccountAdditionView)
+}
