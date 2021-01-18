698ec7ec21e79232796705304b48211e3b10ccac
goktugberkulu <goktugberkulu@users.noreply.github.com>
2021-01-18 16:24:09 +0300

    :recycle: Refactor transaction history header view model

---
+++ Classes/ViewModels/Accounts/Transaction/List/TransactionHistoryHeaderViewModel.swift
@@ -0,0 +1,60 @@
+//
+//  TransactionHistoryHeaderViewModel.swift
+//  algorand
+//
+//  Created by Göktuğ Berk Ulu on 29.06.2020.
+//  Copyright © 2020 hippo. All rights reserved.
+//
+
+import UIKit
+
+class TransactionHistoryHeaderViewModel {
+    private(set) var image: UIImage?
+    private(set) var title: String?
+
+    init(filterOption: TransactionFilterViewController.FilterOption) {
+        setImage(from: filterOption)
+        setTitle(from: filterOption)
+    }
+
+    private func setImage(from filterOption: TransactionFilterViewController.FilterOption) {
+        switch filterOption {
+        case .allTime:
+            image = img("icon-transaction-filter")
+        case .today:
+            image = img("icon-transaction-filter-badge")
+        case .yesterday:
+            image = img("icon-transaction-filter-badge")
+        case .lastWeek:
+            image = img("icon-transaction-filter-badge")
+        case .lastMonth:
+            image = img("icon-transaction-filter-badge")
+        case .customRange:
+            image = img("icon-transaction-filter-badge")
+        }
+    }
+
+    private func setTitle(from filterOption: TransactionFilterViewController.FilterOption) {
+        switch filterOption {
+        case .allTime:
+            title = "contacts-transactions-title".localized
+        case .today:
+            title = "transaction-filter-option-today".localized
+        case .yesterday:
+            title = "transaction-filter-option-yesterday".localized
+        case .lastWeek:
+            title = "transaction-filter-option-week".localized
+        case .lastMonth:
+            title = "transaction-filter-option-month".localized
+        case let .customRange(from, to):
+            if let from = from,
+                let to = to {
+                if from.year == to.year {
+                    title = "\(from.toFormat("MMM dd"))-\(to.toFormat("MMM dd"))"
+                } else {
+                    title = "\(from.toFormat("MMM dd, yyyy"))-\(to.toFormat("MMM dd, yyyy"))"
+                }
+            }
+        }
+    }
+}
