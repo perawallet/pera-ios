909506d102333c03dc6a84e172c47429e484f3cb
goktugberkulu <goktugberkulu@users.noreply.github.com>
2021-01-25 20:57:28 +0300

    :recycle: Refactor transaction unsigned transaction data compose
    Refactored unsigned data composing for transactions below:
    - Asset Transfer
    - Algos Transfer
    - Rekey
    - Asset Addition
    - Asset Removal
    
    Implemented proper interface for each transaction type

---
+++ Classes/Utility/Transaction/DataBuilder/SendAssetTransactionDataBuilder.swift
@@ -0,0 +1,48 @@
+//
+//  SendAssetTransactionDataBuilder.swift
+//  algorand
+//
+//  Created by Göktuğ Berk Ulu on 25.01.2021.
+//  Copyright © 2021 hippo. All rights reserved.
+//
+
+import Foundation
+
+class SendAssetTransactionDataBuilder: TransactionDataBuilder {
+
+    override func composeData() -> Data? {
+        return composeAssetTransactionData()
+    }
+
+    private func composeAssetTransactionData() -> Data? {
+        guard let params = params,
+              let assetTransactionDraft = draft as? AssetTransactionSendDraft,
+              let assetIndex = assetTransactionDraft.assetIndex,
+              let amountDoubleValue = assetTransactionDraft.amount,
+              let toAddress = assetTransactionDraft.toAccount else {
+            delegate?.transactionDataBuilder(self, didFailedComposing: .inapp(TransactionError.other))
+            return nil
+        }
+
+        if !isValidAddress(toAddress.trimmed) {
+            return nil
+        }
+
+        var transactionError: NSError?
+        let draft = AssetTransactionDraft(
+            from: assetTransactionDraft.from,
+            toAccount: toAddress.trimmed,
+            transactionParams: params,
+            amount: amountDoubleValue.toFraction(of: assetTransactionDraft.assetDecimalFraction),
+            assetIndex: assetIndex,
+            note: assetTransactionDraft.note?.data(using: .utf8)
+        )
+
+        guard let transactionData = algorandSDK.sendAsset(with: draft, error: &transactionError) else {
+            delegate?.transactionDataBuilder(self, didFailedComposing: .inapp(TransactionError.sdkError(error: transactionError)))
+            return nil
+        }
+
+        return transactionData
+    }
+}
