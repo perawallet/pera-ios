83d473c7ea3c7634d54fbd8b270e5b5855e79d39
goktugberkulu <goktugberkulu@users.noreply.github.com>
2021-03-18 23:18:14 +0300

    :sparkles: Implement input suggestions view

---
+++ Classes/Data/DataProviders/Authentication/Recover/Suggestion/InputSuggestionsDataController.swift
@@ -0,0 +1,76 @@
+// Copyright 2019 Algorand, Inc.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//    http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//
+//   InputSuggestionsDataController.swift
+
+import Foundation
+
+class InputSuggestionsDataController: NSObject {
+
+    weak var delegate: InputSuggestionsDataControllerDelegate?
+
+    private let suggestionCount = 3
+
+    private var allSuggestions: [String] = []
+
+    private var currentSuggestions: [String]
+
+    override init() {
+        currentSuggestions = [String](repeating: "", count: suggestionCount)
+        super.init()
+        readSuggestionsFromFile()
+    }
+
+    private func readSuggestionsFromFile() {
+        if let filepath = Bundle.main.path(forResource: "mnemonics", ofType: "txt") {
+            do {
+                let contents = try String(contentsOfFile: filepath)
+                allSuggestions = contents.components(separatedBy: "\n")
+            } catch {
+                delegate?.inputSuggestionsDataController(self, didFailedWith: .mnemonicReadingFailed)
+            }
+        }
+    }
+}
+
+extension InputSuggestionsDataController {
+    func suggestion(at index: Int) -> String? {
+        return currentSuggestions[safe: index]
+    }
+
+    func findTopSuggestions(for text: String) {
+        let filteredSuggestions = allSuggestions.filter { $0.contains(text) }
+        for i in 0...suggestionCount - 1 {
+            currentSuggestions[i] = filteredSuggestions[safe: i] ?? ""
+        }
+    }
+
+    var currentSuggestionCount: Int {
+        return currentSuggestions.filter { !$0.isEmpty }.count
+    }
+}
+
+extension InputSuggestionsDataController {
+    enum SuggestionError: Error {
+        case mnemonicReadingFailed
+    }
+}
+
+protocol InputSuggestionsDataControllerDelegate: class {
+    func inputSuggestionsDataController(
+        _ inputSuggestionsDataController: InputSuggestionsDataController,
+        didFailedWith error: InputSuggestionsDataController.SuggestionError
+    )
+}
