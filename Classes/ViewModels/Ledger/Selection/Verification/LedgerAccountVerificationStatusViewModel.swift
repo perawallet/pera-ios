e8a09ceca116b4b8165125ae46f8c34574cfbfa5
goktugberkulu <goktugberkulu@users.noreply.github.com>
2021-04-28 00:57:22 +0300

    :sparkles: Implement ledger account verification status view

---
+++ Classes/ViewModels/Ledger/Selection/Verification/LedgerAccountVerificationStatusViewModel.swift
@@ -0,0 +1,62 @@
+// Copyright 2019 Algorand, Inc.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//    http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//
+//   LedgerAccountVerificationStatusViewModel.swift
+
+import UIKit
+
+class LedgerAccountVerificationStatusViewModel {
+
+    private(set) var address: String?
+    private(set) var backgroundColor: UIColor?
+    private(set) var borderColor: UIColor?
+    private(set) var verificationStatusViewModel: VerificationStatusViewModel?
+
+    init(account: Account, status: LedgerVerificationStatus) {
+        setAddress(from: account)
+        setBackgroundColor(from: status)
+        setBorderColor(from: status)
+        setVerificationStatusViewModel(from: status)
+    }
+
+    private func setAddress(from account: Account) {
+        address = account.address
+    }
+
+    private func setBackgroundColor(from status: LedgerVerificationStatus) {
+        switch status {
+        case .awaiting:
+            backgroundColor = Colors.Main.white
+        case .pending:
+            backgroundColor = .clear
+        case .unverified:
+            backgroundColor = Colors.General.error
+        case .verified:
+            backgroundColor = Colors.Main.white
+        }
+    }
+
+    private func setBorderColor(from status: LedgerVerificationStatus) {
+        if status == .awaiting {
+            borderColor = Colors.Main.yellow700
+        } else {
+            borderColor = .clear
+        }
+    }
+
+    private func setVerificationStatusViewModel(from status: LedgerVerificationStatus) {
+        verificationStatusViewModel = VerificationStatusViewModel(status: status)
+    }
+}
