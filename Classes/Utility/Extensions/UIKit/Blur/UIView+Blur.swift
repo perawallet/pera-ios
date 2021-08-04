e06d8030bb6dbca986b452811ec9ed18cd2ef69f
bahadirseyfi <bahadirsoftware@gmail.com>
2021-08-05 00:36:53 +0300

    Add blur extension
    The element it will take with the setup function will be blurred.

---
+++ Classes/Utility/Extensions/UIKit/Blur/UIView+Blur.swift
@@ -0,0 +1,112 @@
+// Copyright 2019 Algorand, Inc.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//    http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//
+//   UIView+Blur.swift
+
+import UIKit
+
+extension UIView {
+    
+    enum AssociatedKeys {
+        static var descriptiveName = "AssociatedKeys.DescriptiveName.blurView"
+    }
+    
+    private (set) var blurView: BlurView {
+        get {
+            if let blurView = objc_getAssociatedObject(
+                self,
+                &AssociatedKeys.descriptiveName
+            ) as? BlurView {
+                return blurView
+            }
+            self.blurView = BlurView(to: self)
+            return self.blurView
+        }
+        set(blurView) {
+            objc_setAssociatedObject(
+                self,
+                &AssociatedKeys.descriptiveName,
+                blurView,
+                .OBJC_ASSOCIATION_RETAIN_NONATOMIC
+            )
+        }
+    }
+    
+    class BlurView {
+        
+        private var superview: UIView
+        private var blur: UIVisualEffectView?
+        private var editing: Bool = false
+        private (set) var blurContentView: UIView?
+        private (set) var vibrancyContentView: UIView?
+        
+        var animationDuration: TimeInterval = 0.1
+
+        var style: UIBlurEffect.Style = .dark
+        
+        var alpha: CGFloat = 1
+        
+        init(to view: UIView) {
+            self.superview = view
+        }
+        
+        func setup() -> Self {
+            self.editing = true
+            self.editing = false
+            
+            return self
+        }
+        
+        func enable(isHidden: Bool = false) {
+            if blur == nil {
+                applyBlurEffect()
+            }
+            
+            self.blur?.isHidden = isHidden
+        }
+        
+        private func applyBlurEffect() {
+            blur?.removeFromSuperview()
+            applyBlurEffect(
+                style: style,
+                blurAlpha: alpha
+            )
+        }
+        
+        private func applyBlurEffect(style: UIBlurEffect.Style,
+                                     blurAlpha: CGFloat) {
+            superview.backgroundColor = UIColor.clear
+            let blurEffect = UIBlurEffect(style: style)
+            let blurEffectView = UIVisualEffectView(effect: blurEffect)
+            let vibrancyEffect = UIVibrancyEffect(blurEffect: blurEffect)
+            let vibrancyView = UIVisualEffectView(effect: vibrancyEffect)
+            blurEffectView.contentView.addSubview(vibrancyView)
+            blurEffectView.alpha = blurAlpha
+            superview.insertSubview(blurEffectView, at: 0)
+            
+            blurEffectView.snp.makeConstraints { make in
+                make.edges.equalToSuperview()
+            }
+            
+            vibrancyView.snp.makeConstraints { make in
+                make.edges.equalToSuperview()
+            }
+            
+            self.blur = blurEffectView
+            self.blurContentView = blurEffectView.contentView
+            self.vibrancyContentView = vibrancyView.contentView
+        }
+    }
+}
