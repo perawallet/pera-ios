247bac6b76182220f7d9c11a18667cd7a8739902
Ege Eke <egeeke2001@gmail.com>
2021-08-31 14:44:27 +0300

    ✨ Implement Button

---
+++ Classes/Views/Common/Button/Button.swift
@@ -0,0 +1,82 @@
+// Copyright 2019 Algorand, Inc.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//    http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//
+//   Button.swift
+
+import UIKit
+import Macaroon
+
+final class Button: UIButton {
+    func customize(_ theme: ButtonTheme) {
+        customizeView(theme)
+        customizeBackground(theme)
+        customizeLabel(theme)
+    }
+
+    func prepareLayout(_ layoutSheet: NoLayoutSheet) {}
+
+    func customizeAppearance(_ styleSheet: NoStyleSheet) {}
+
+    func bindData(_ viewModel: ButtonViewModel?) {
+        bindTitle(viewModel)
+        bindIcon(viewModel)
+    }
+}
+
+extension Button {
+    private func customizeView(_ theme: ButtonTheme) {
+        layer.draw(corner: theme.corner)
+        layer.masksToBounds = true
+    }
+
+    private func customizeBackground(_ theme: ButtonTheme) {
+        setBackgroundColor(theme.backgroundColorSet.color, for: .normal)
+        setBackgroundColor(theme.backgroundColorSet.disabled, for: .disabled)
+    }
+
+    private func customizeLabel(_ theme: ButtonTheme) {
+        titleLabel?.customizeAppearance(theme.label)
+        setTitleColor(theme.titleColorSet.color, for: .normal)
+        setTitleColor(theme.titleColorSet.disabled, for: .disabled)
+
+        titleEdgeInsets = UIEdgeInsets(theme.titleEdgeInsets)
+
+        imageView?.customizeAppearance(theme.icon)
+    }
+}
+
+extension Button {
+    private func bindTitle(_ viewModel: ButtonViewModel?) {
+        setTitle(viewModel?.title?.string, for: .normal)
+    }
+
+    private func bindIcon(_ viewModel: ButtonViewModel?) {
+        setImage(viewModel?.iconSet?.image, for: .normal)
+        setImage(viewModel?.iconSet?.disabled, for: .disabled)
+    }
+}
+
+extension Button {
+    private func setBackgroundColor(_ color: UIColor?, for state: UIControl.State) {
+        guard let color = color else { return }
+
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 1, height: 1))
+        let colorImage = renderer.image { _ in
+            color.setFill()
+            UIBezierPath(rect: CGRect(x: 0, y: 0, width: 1, height: 1)).fill()
+        }
+        setBackgroundImage(colorImage, for: state)
+    }
+}
