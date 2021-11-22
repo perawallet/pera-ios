33c1c4607ddf517496be9fabf4e7f85ebab0b8db
Ege Eke <egeeke2001@gmail.com>
2021-11-22 18:41:09 +0300

    🔥 Remove `CardModalPresented`
    - BottomSheetTransition replaced it.

--- Classes/Accounts/Options/Edit/Screens/EditAccountViewController.swift
+++ Classes/Accounts/Options/Edit/Screens/EditAccountViewController.swift
@@ -16,23 +16,11 @@
 //  EditAccountViewController.swift
 
 import UIKit
-import SnapKit
 
+// <todo>: Handle keyboard 
 final class EditAccountViewController: BaseViewController {
     private lazy var theme = Theme()
     private lazy var editAccountView = EditAccountView()
-    
-    private lazy var keyboard = Keyboard()
-    private var contentViewBottomConstraint: Constraint?
-
-    private lazy var modalSize: ModalSize = {
-        let keyboardHeight = keyboard.height ?? 0
-        let size = CGSize(
-            width: view.bounds.width,
-            height: keyboardHeight + 244
-        )
-        return .custom(size)
-    }()
 
     private let account: Account
 
@@ -48,21 +36,13 @@ final class EditAccountViewController: BaseViewController {
     
     override func setListeners() {
         editAccountView.delegate = self
-
-        NotificationCenter.default.addObserver(
-            self,
-            selector: #selector(didReceive(keyboardWillShow:)),
-            name: UIResponder.keyboardWillShowNotification,
-            object: nil
-        )
     }
     
     override func prepareLayout() {
         editAccountView.customize(theme.editAccountViewTheme)
         view.addSubview(editAccountView)
         editAccountView.snp.makeConstraints {
-            $0.leading.trailing.top.equalToSuperview()
-            contentViewBottomConstraint = $0.bottom.equalToSuperview().inset(0).constraint
+            $0.edges.equalToSuperview()
         }
     }
 
@@ -89,37 +69,6 @@ extension EditAccountViewController {
         account.name = name
         session?.updateName(name, for: account.address)
         dismissScreen()
-    }
-}
-
-extension EditAccountViewController {
-    @objc
-    private func didReceive(keyboardWillShow notification: Notification) {
-        if !UIApplication.shared.isActive {
-            return
-        }
-        
-        let kbHeight = notification.keyboardHeight ?? 0
-        
-        keyboard.height = kbHeight
-        
-        let inset = kbHeight - self.view.safeAreaInsets.bottom
-        let duration = notification.keyboardAnimationDuration
-        let curve = notification.keyboardAnimationCurve
-        let curveAnimationOption = UIView.AnimationOptions(rawValue: UInt(curve.rawValue >> 16))
-        
-        self.contentViewBottomConstraint?.update(inset: inset)
-        
-        UIView.animate(
-            withDuration: duration,
-            delay: 0,
-            options: [curveAnimationOption],
-            animations: {
-                self.modalPresenter?.changeModalSize(to: self.modalSize, animated: false)
-                self.view.layoutIfNeeded()
-            },
-            completion: nil
-        )
     }
 }
 
