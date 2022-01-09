b4f73a1e3bbf5515340f1c13f87e639f9fb8825b
Ege Eke <egeeke2001@gmail.com>
2022-01-09 14:00:25 +0300

    🐛 Fix should return in contact add & edit

--- Classes/Contacts/Edit/Views/EditContactView.swift
+++ Classes/Contacts/Edit/Views/EditContactView.swift
@@ -129,15 +129,13 @@ extension EditContactView {
 }
 
 extension EditContactView {
-    func createAddressTextInput(
+    func createAccountNameTextInput(
         placeholder: String,
         floatingPlaceholder: String?
     ) -> FloatingTextInputFieldView {
-        /// <todo>
-        /// Multi line is not supported.
         let view = FloatingTextInputFieldView()
         let textInputBaseStyle: TextInputStyle = [
-            .font(Fonts.DMSans.regular.make(15, .body)),
+            .font(Fonts.DMSans.regular.make(15)),
             .tintColor(AppColors.Components.Text.main),
             .textColor(AppColors.Components.Text.main),
             .returnKeyType(.next)
@@ -149,6 +147,7 @@ extension EditContactView {
                 placeholder: placeholder,
                 floatingPlaceholder: floatingPlaceholder
             )
+        view.delegate = self
         view.customize(theme)
         view.snp.makeConstraints {
             $0.greaterThanHeight(48)
@@ -156,13 +155,15 @@ extension EditContactView {
         return view
     }
 
-    func createAccountNameTextInput(
+    func createAddressTextInput(
         placeholder: String,
         floatingPlaceholder: String?
     ) -> FloatingTextInputFieldView {
+        /// <todo>
+        /// Multi line is not supported.
         let view = FloatingTextInputFieldView()
         let textInputBaseStyle: TextInputStyle = [
-            .font(Fonts.DMSans.regular.make(15, .body)),
+            .font(Fonts.DMSans.regular.make(15)),
             .tintColor(AppColors.Components.Text.main),
             .textColor(AppColors.Components.Text.main),
             .returnKeyType(.done)
@@ -175,6 +176,7 @@ extension EditContactView {
                 floatingPlaceholder: floatingPlaceholder
             )
         view.customize(theme)
+        view.delegate = self
         view.snp.makeConstraints {
             $0.greaterThanHeight(48)
         }
@@ -190,8 +192,22 @@ extension EditContactView: ViewModelBindable {
     }
 }
 
+extension EditContactView: FloatingTextInputFieldViewDelegate {
+    func floatingTextInputFieldViewShouldReturn(_ view: FloatingTextInputFieldView) -> Bool {
+        guard let delegate = delegate else {
+            return true
+        }
+
+        return delegate.editContactViewInputFieldViewShouldReturn(self, inputFieldView: view)
+    }
+}
+
 protocol EditContactViewDelegate: AnyObject {
     func editContactViewDidTapDeleteButton(_ editContactView: EditContactView)
     func editContactViewDidTapAddImageButton(_ editContactView: EditContactView)
     func editContactViewDidTapQRCodeButton(_ editContactView: EditContactView)
+    func editContactViewInputFieldViewShouldReturn(
+        _ editContactView: EditContactView,
+        inputFieldView: FloatingTextInputFieldView
+    ) -> Bool
 }
