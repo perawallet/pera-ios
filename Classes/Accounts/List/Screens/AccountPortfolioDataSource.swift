c9d848b2f2ae1fd6ab756ec241d480a978ddc47e
goktugberkulu <goktugberkulu@gmail.com>
2022-01-09 03:41:14 +0300

    ✨ Implement accounts portfolio screen

---
+++ Classes/Accounts/List/Screens/AccountPortfolioDataSource.swift
@@ -0,0 +1,179 @@
+// Copyright 2019 Algorand, Inc.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//    http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//
+//   AccountPortfolioDataSource.swift
+
+import UIKit
+
+final class AccountPortfolioDataSource: NSObject {
+    lazy var handlers = Handlers()
+
+    typealias DataSource = UICollectionViewDiffableDataSource<AccountPortfolioSection, AccountPortfolioItem>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<AccountPortfolioSection, AccountPortfolioItem>
+
+    private lazy var currentSnapshot = Snapshot()
+
+    private weak var listView: UICollectionView?
+    private let session: Session
+
+    init(listView: UICollectionView, session: Session) {
+        self.listView = listView
+        self.session = session
+    }
+
+    private(set) lazy var dataSource: DataSource = {
+        guard let listView = listView else {
+            fatalError()
+        }
+
+        let dataSource = DataSource(collectionView: listView) {
+            [weak self] collectionView, indexPath, identifier in
+                guard let self = self else {
+                    return nil
+                }
+
+                switch identifier {
+                case .portfolio:
+                    let cell = collectionView.dequeue(AccountPortfolioCell.self, at: indexPath)
+                    cell.bindData(AccountPortfolioViewModel(self.session.accounts))
+                    return cell
+                case .announcement:
+                    let cell = collectionView.dequeue(AnnouncementBannerCell.self, at: indexPath)
+                    cell.bindData(AnnouncementBannerViewModel())
+                    return cell
+                case let .standardAccount(account):
+                    let cell = collectionView.dequeue(AccountPreviewCell.self, at: indexPath)
+                    cell.bindData(AccountPreviewViewModel(from: account))
+                    return cell
+                case let .watchAccount(account):
+                    let cell = collectionView.dequeue(AccountPreviewCell.self, at: indexPath)
+                    cell.bindData(AccountPreviewViewModel(from: account))
+                    return cell
+                }
+            }
+
+        dataSource.supplementaryViewProvider = { collectionView, kind, indexPath in
+            guard kind == UICollectionView.elementKindSectionHeader,
+                  let section = AccountPortfolioSection(rawValue: indexPath.section),
+                  section == .standardAccount || section == .watchAccount else {
+                return nil
+            }
+
+            let title = section == .watchAccount ? "portfolio-title-watchlist".localized : "portfolio-title-accounts".localized
+            let view = collectionView.dequeueHeader(SingleLineTitleActionHeaderView.self, at: indexPath)
+            view.bindData(
+                SingleLineTitleActionViewModel(
+                    item: SingleLineIconTitleItem(
+                        icon: "icon-options",
+                        title: .string(title)
+                    )
+                )
+            )
+
+            view.handlers.didHandleAction = { [weak self] in
+                guard let self = self else {
+                    return
+                }
+
+                self.handlers.didSelectSection?(section)
+            }
+
+            return view
+        }
+
+        return dataSource
+    }()
+}
+
+extension AccountPortfolioDataSource {
+    func applySnapshot(
+        animatingDifferences: Bool = true
+    ) {
+        guard !session.accounts.isEmpty else {
+            return
+        }
+
+        var snapshot = Snapshot()
+
+        snapshot.appendSections([.portfolio])
+        snapshot.appendItems(
+            [.portfolio],
+            toSection: .portfolio
+        )
+
+        snapshot.appendSections([.announcement])
+
+        /// <todo> Add Announcement Section
+
+        let nonWatchAccounts = session.accounts.filter { $0.type != .watch }
+
+        if !nonWatchAccounts.isEmpty {
+            snapshot.appendSections([.standardAccount])
+
+            var nonWatchAccountSectionItems: [AccountPortfolioItem] = []
+            nonWatchAccounts.forEach {
+                nonWatchAccountSectionItems.append(.standardAccount(account: $0))
+            }
+
+            snapshot.appendItems(
+                nonWatchAccountSectionItems,
+                toSection: .standardAccount
+            )
+        }
+
+        let watchAccounts = session.accounts.filter { $0.type == .watch }
+
+        if !watchAccounts.isEmpty {
+            snapshot.appendSections([.watchAccount])
+
+            var watchAccountSectionItems: [AccountPortfolioItem] = []
+            watchAccounts.forEach {
+                watchAccountSectionItems.append(.watchAccount(account: $0))
+            }
+
+            snapshot.appendItems(
+                watchAccountSectionItems,
+                toSection: .watchAccount
+            )
+        }
+
+        dataSource.apply(
+            snapshot,
+            animatingDifferences: animatingDifferences
+        )
+
+        currentSnapshot = snapshot
+    }
+}
+
+extension AccountPortfolioDataSource {
+    struct Handlers {
+        var didSelectSection: ((AccountPortfolioSection) -> Void)?
+    }
+}
+
+enum AccountPortfolioSection: Int, Hashable {
+    case portfolio
+    case announcement
+    case standardAccount
+    case watchAccount
+}
+
+enum AccountPortfolioItem: Hashable {
+    case portfolio
+    case announcement
+    case standardAccount(account: Account)
+    case watchAccount(account: Account)
+}
--- Classes/Accounts/List/Screens/AccountPortfolioViewController.swift
+++ Classes/Accounts/List/Screens/AccountPortfolioViewController.swift
@@ -20,9 +20,17 @@ import UIKit
 import MacaroonUIKit
 
 final class AccountPortfolioViewController: BaseViewController {
+    private lazy var listLayout = AccountPortfolioListLayout(session: session!)
+    private lazy var portfolioDataSource = AccountPortfolioDataSource(listView: listView, session: session!)
+    private lazy var accountManager = AccountManager(api: api!)
+    private lazy var modalTransition = BottomSheetTransition(presentingViewController: self)
+    private lazy var pushNotificationController = PushNotificationController(api: api!, bannerController: bannerController)
+    
+    private let onceWhenViewDidAppear = Once()
 
-    typealias DataSource = UICollectionViewDiffableDataSource<AccountPortfolioSection, AccountPortfolioItem>
-    typealias Snapshot = NSDiffableDataSourceSnapshot<AccountPortfolioSection, AccountPortfolioItem>
+    override var name: AnalyticsScreenName? {
+        return .accounts
+    }
 
     private lazy var listView: UICollectionView = {
         let flowLayout = UICollectionViewFlowLayout()
@@ -36,47 +44,68 @@ final class AccountPortfolioViewController: BaseViewController {
         return collectionView
     }()
 
-    private lazy var dataSource: DataSource = {
-        let dataSource = DataSource(collectionView: listView) {
-            [weak self] collectionView, indexPath, identifier in
-                guard let self = self else {
-                    return nil
-                }
+    override func configureNavigationBarAppearance() {
+        addBarButtons()
+    }
 
-                switch identifier {
-                case .empty:
-                    let cell = collectionView.dequeue(AccountPreviewCell.self, at: indexPath)
-                    return cell
-                case .portfolio:
-                    let cell = collectionView.dequeue(AccountPortfolioCell.self, at: indexPath)
-                    return cell
-                case .announcement:
-                    let cell = collectionView.dequeue(AnnouncementBannerCell.self, at: indexPath)
-                    return cell
-                case .standardAccount,
-                        .watchAccount:
-                    let cell = collectionView.dequeue(AccountPreviewCell.self, at: indexPath)
-                    return cell
-                }
-            }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        fetchAccountsIfNeeded()
 
-        dataSource.supplementaryViewProvider = { collectionView, kind, indexPath in
-            guard kind == UICollectionView.elementKindSectionHeader,
-                  let section = AccountPortfolioSection(rawValue: indexPath.section),
-                  section == .standardAccount || section == .watchAccount else {
-                return nil
-            }
-
-            let view = collectionView.dequeueHeader(SingleLineTitleActionHeaderView.self, at: indexPath)
-            return view
+        DispatchQueue.main.async {
+            UIApplication.shared.appDelegate?.validateAccountManagerFetchPolling()
         }
 
-        return dataSource
-    }()
+        portfolioDataSource.applySnapshot(animatingDifferences: false)
+
+        pushNotificationController.requestAuthorization()
+        pushNotificationController.sendDeviceDetails()
+
+        requestAppReview()
+        presentPasscodeFlowIfNeeded()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        reconnectToOldWCSessions()
+        connectToWCSessionRequestFromDeeplink()
+    }
+
+    private func fetchAccountsIfNeeded() {
+        guard let session = session,
+              let user = session.authenticatedUser,
+              !session.hasPassword(),
+              !user.accounts.isEmpty else {
+            return
+        }
+
+        loadingController?.startLoadingWithMessage("title-loading".localized)
+        accountManager.fetchAllAccounts(isVerifiedAssetsIncluded: true) {
+            self.loadingController?.stopLoadingAfter(seconds: 1, on: .main) { [weak self] in
+                self?.portfolioDataSource.applySnapshot()
+            }
+        }
+    }
+
+    override func customizeTabBarAppearence() {
+        isTabBarHidden = false
+    }
 
     override func prepareLayout() {
         super.prepareLayout()
         addListView()
+    }
+
+    override func setListeners() {
+        super.setListeners()
+        setListSelectionListeners()
+        setSectionSelectionListeners()
+    }
+
+    override func linkInteractors() {
+        super.linkInteractors()
+        listView.delegate = listLayout
+        listView.dataSource = portfolioDataSource.dataSource
     }
 }
 
@@ -87,30 +116,275 @@ extension AccountPortfolioViewController {
             $0.edges.equalToSuperview()
         }
     }
-}
 
-extension AccountPortfolioViewController {
-    private func applySnapshot(animatingDifferences: Bool = true) {
-      let snapshot = Snapshot()
-      // snapshot.appendSections(sections)
-      // sections.forEach { section in
-        // snapshot.appendItems(section.videos, toSection: section)
-      // }
-      dataSource.apply(snapshot, animatingDifferences: animatingDifferences)
+    private func addBarButtons() {
+        let notificationBarButtonItem = ALGBarButtonItem(kind: .notification) { [weak self] in
+            guard let self = self else {
+                return
+            }
+
+            self.open(.notifications, by: .push)
+        }
+
+        let qrBarButtonItem = ALGBarButtonItem(kind: .qr) { [weak self] in
+            guard let self = self else {
+                return
+            }
+
+            let qrScannerViewController = self.open(.qrScanner(canReadWCSession: true), by: .push) as? QRScannerViewController
+            qrScannerViewController?.delegate = self
+        }
+
+        let addBarButtonItem = ALGBarButtonItem(kind: .add) { [weak self] in
+            guard let self = self else {
+                return
+            }
+
+            self.open(
+                .welcome(flow: .addNewAccount(mode: .none)),
+                by: .customPresent(presentationStyle: .fullScreen, transitionStyle: nil, transitioningDelegate: nil)
+            )
+        }
+
+        leftBarButtonItems = [notificationBarButtonItem]
+        rightBarButtonItems = [addBarButtonItem, qrBarButtonItem]
+    }
+
+    private func setListSelectionListeners() {
+        listLayout.handlers.didSelectAccount = { [weak self] account in
+            guard let self = self else {
+                return
+            }
+
+            self.open(.accountDetail(account: account), by: .push)
+        }
+    }
+
+    private func setSectionSelectionListeners() {
+        portfolioDataSource.handlers.didSelectSection = { [weak self] section in
+            guard let self = self else {
+                return
+            }
+
+            let accountType: AccountType = section == .watchAccount ? .watch : .standard
+
+            let controller = self.modalTransition.perform(
+                .accountListOptions(accountType: accountType)
+            ) as? AccountListOptionsViewController
+
+            controller?.handlers.didSelect = { [weak self] option, accountType in
+                guard let self = self else {
+                    return
+                }
+
+                switch option {
+                case .add:
+                    self.open(
+                        .welcome(flow: .addNewAccount(mode: .none)),
+                        by: .customPresent(presentationStyle: .fullScreen, transitionStyle: nil, transitioningDelegate: nil)
+                    )
+                case .arrange:
+                    self.open(.orderAccountList(accountType: accountType), by: .present)
+                }
+            }
+        }
     }
 }
 
-enum AccountPortfolioSection: Int, Hashable {
-    case portfolio
-    case announcement
-    case standardAccount
-    case watchAccount
+extension AccountPortfolioViewController {
+    private func requestAppReview() {
+        asyncMain(afterDuration: 1.0) {
+            AlgorandAppStoreReviewer().requestReviewIfAppropriate()
+        }
+    }
+
+    private func presentPasscodeFlowIfNeeded() {
+        guard let session = session,
+              !session.hasPassword() else {
+            return
+        }
+
+        var passcodeSettingDisplayStore = PasscodeSettingDisplayStore()
+
+        if !passcodeSettingDisplayStore.hasPermissionToAskAgain {
+            return
+        }
+
+        passcodeSettingDisplayStore.increaseAppOpenCount()
+
+        if passcodeSettingDisplayStore.shouldAskForPasscode {
+            let controller = open(
+                .tutorial(flow: .none, tutorial: .passcode),
+                by: .customPresent(presentationStyle: .fullScreen, transitionStyle: nil, transitioningDelegate: nil)
+            ) as? TutorialViewController
+            controller?.uiHandlers.didTapDontAskAgain = { tutorialViewController in
+                tutorialViewController.dismissScreen()
+                var passcodeSettingDisplayStore = PasscodeSettingDisplayStore()
+                passcodeSettingDisplayStore.disableAskingPasscode()
+            }
+        }
+    }
 }
 
-enum AccountPortfolioItem: Hashable {
-    case empty
-    case portfolio
-    case announcement
-    case standardAccount
-    case watchAccount
+extension AccountPortfolioViewController {
+    private func reconnectToOldWCSessions() {
+        onceWhenViewDidAppear.execute {
+            asyncMain(afterDuration: 2.0) { [weak self] in
+                guard let self = self else {
+                    return
+                }
+
+                self.walletConnector.reconnectToSavedSessionsIfPossible()
+            }
+        }
+    }
+
+    private func registerWCRequests() {
+        let wcRequestHandler = TransactionSignRequestHandler()
+        if let rootViewController = UIApplication.shared.rootViewController() {
+            wcRequestHandler.delegate = rootViewController
+        }
+        walletConnector.register(for: wcRequestHandler)
+    }
+
+    private func connectToWCSessionRequestFromDeeplink() {
+        if let appDelegate = UIApplication.shared.appDelegate,
+           let incominWCSession = appDelegate.incomingWCSessionRequest {
+            walletConnector.delegate = self
+
+            asyncMain(afterDuration: 2.0) { [weak self] in
+                guard let self = self else {
+                    return
+                }
+
+                self.walletConnector.connect(to: incominWCSession)
+            }
+
+            appDelegate.resetWCSessionRequest()
+        }
+    }
+}
+
+extension AccountPortfolioViewController: WalletConnectorDelegate {
+    func walletConnector(
+        _ walletConnector: WalletConnector,
+        shouldStart session: WalletConnectSession,
+        then completion: @escaping WalletConnectSessionConnectionCompletionHandler
+    ) {
+        guard let accounts = self.session?.accounts,
+              accounts.contains(where: { $0.type != .watch }) else {
+            bannerController?.presentErrorBanner(
+                title: "title-error".localized,
+                message: "wallet-connect-session-error-no-account".localized
+            )
+            return
+        }
+
+        modalTransition.perform(.wcConnectionApproval(walletConnectSession: session, delegate: self, completion: completion))
+    }
+
+    func walletConnector(_ walletConnector: WalletConnector, didConnectTo session: WCSession) {
+        walletConnector.saveConnectedWCSession(session)
+    }
+}
+
+extension AccountPortfolioViewController: WCConnectionApprovalViewControllerDelegate {
+    func wcConnectionApprovalViewControllerDidApproveConnection(_ wcConnectionApprovalViewController: WCConnectionApprovalViewController) {
+        wcConnectionApprovalViewController.dismissScreen()
+    }
+
+    func wcConnectionApprovalViewControllerDidRejectConnection(_ wcConnectionApprovalViewController: WCConnectionApprovalViewController) {
+        wcConnectionApprovalViewController.dismissScreen()
+    }
+}
+
+extension AccountPortfolioViewController {
+    private func presentOptions(for account: Account) {
+       // modalTransition.perform(.options(account: account, delegate: self))
+    }
+}
+
+extension AccountPortfolioViewController: QRScannerViewControllerDelegate {
+    func qrScannerViewController(_ controller: QRScannerViewController, didRead qrText: QRText, completionHandler: EmptyHandler?) {
+        switch qrText.mode {
+        case .address:
+            open(.addContact(address: qrText.address, name: qrText.label), by: .push)
+        case .algosRequest:
+            guard let address = qrText.address,
+                let amount = qrText.amount else {
+                return
+            }
+
+            open(
+                .sendAlgosTransactionPreview(
+                    account: nil,
+                    receiver: .address(address: address, amount: "\(amount)"),
+                    isSenderEditable: true,
+                    qrText: qrText
+                ),
+                by: .customPresent(
+                    presentationStyle: .fullScreen,
+                    transitionStyle: nil,
+                    transitioningDelegate: nil
+                )
+            )
+        case .assetRequest:
+            guard let address = qrText.address,
+                let amount = qrText.amount,
+                let assetId = qrText.asset else {
+                return
+            }
+
+            var asset: AssetDetail?
+
+            for account in session!.accounts {
+                for assetDetail in account.assetDetails where assetDetail.id == assetId {
+                    asset = assetDetail
+                    break
+                }
+            }
+
+            guard let assetDetail = asset else {
+                let assetAlertDraft = AssetAlertDraft(
+                    account: nil,
+                    assetIndex: assetId,
+                    assetDetail: nil,
+                    title: "asset-support-your-add-title".localized,
+                    detail: "asset-support-your-add-message".localized,
+                    actionTitle: "title-approve".localized,
+                    cancelTitle: "title-cancel".localized
+                )
+
+                modalTransition.perform(.assetActionConfirmation(assetAlertDraft: assetAlertDraft))
+                return
+            }
+
+            open(
+                .sendAssetTransactionPreview(
+                    account: nil,
+                    receiver: .address(
+                        address: address,
+                        amount: amount
+                            .assetAmount(fromFraction: assetDetail.fractionDecimals)
+                            .toFractionStringForLabel(fraction: assetDetail.fractionDecimals)
+                    ),
+                    assetDetail: assetDetail,
+                    isSenderEditable: false,
+                    isMaxTransaction: false,
+                    qrText: qrText
+                ),
+                by: .push
+            )
+        case .mnemonic:
+            break
+        }
+    }
+
+    func qrScannerViewController(_ controller: QRScannerViewController, didFail error: QRScannerError, completionHandler: EmptyHandler?) {
+        displaySimpleAlertWith(title: "title-error".localized, message: "qr-scan-should-scan-valid-qr".localized) { _ in
+            if let handler = completionHandler {
+                handler()
+            }
+        }
+    }
 }
