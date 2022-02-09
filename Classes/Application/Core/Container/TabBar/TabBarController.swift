28a975f0441daef5129490f2ab1c2752969dd30b
Ömer Aslan <omeremreaslan@gmail.com>
2022-02-09 13:15:01 +0300

    🐛Fix dismissal issue on send flow

--- Classes/Application/Core/Container/TabBar/TabBarController.swift
+++ Classes/Application/Core/Container/TabBar/TabBarController.swift
@@ -238,7 +238,6 @@ extension TabBarController {
             .accountSelection(transactionAction: action),
             by: .present
         )
-        controller?.navigationController?.isModalInPresentation = true
         (controller as? SelectAccountViewController)?.delegate = self
         
         switch action {
