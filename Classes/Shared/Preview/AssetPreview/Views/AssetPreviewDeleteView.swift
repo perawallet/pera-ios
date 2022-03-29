2480eaf3ce3f98d974eb6c5e764a752de197672e
Ahmet C. Sahin <sahin.ahmetcemal@gmail.com>
2022-03-29 17:17:18 +0300

    💄 Change Asset Preview Delete Cell layout

--- Classes/Shared/Preview/AssetPreview/Views/AssetPreviewDeleteView.swift
+++ Classes/Shared/Preview/AssetPreview/Views/AssetPreviewDeleteView.swift
@@ -100,6 +100,9 @@ extension AssetPreviewDeleteView {
         assetTitleHorizontalStackView.addArrangedSubview(primaryAssetTitleLabel)
     }
     private func addSecondaryImage(_ theme: AssetPreviewDeleteViewTheme) {
+        secondaryImageView.snp.makeConstraints {
+            $0.lessThanSize(theme.verifyImageSize)
+        }
         assetTitleHorizontalStackView.addArrangedSubview(secondaryImageView)
     }
     private func addSecondaryAssetTitleLabel(_ theme: AssetPreviewDeleteViewTheme) {
@@ -132,6 +135,7 @@ extension AssetPreviewDeleteView {
             $0.trailing.equalTo(deleteButton.snp.leading).offset(-theme.assetValueTrailingPadding)
             $0.leading.equalTo(assetTitleVerticalStackView.snp.trailing).offset(theme.horizontalPadding)
             $0.centerY.equalTo(assetTitleVerticalStackView.snp.centerY)
+            $0.width.greaterThanOrEqualToSuperview().multipliedBy(theme.assetValueMinRatio)
         }
 
         addPrimaryAssetValueLabel(theme)
@@ -173,4 +177,3 @@ extension AssetPreviewDeleteView: ViewModelBindable {
 protocol AssetPreviewDeleteViewDelegate: AnyObject {
     func assetPreviewDeleteViewDidDelete(_ assetPreviewDeleteView: AssetPreviewDeleteView)
 }
-
