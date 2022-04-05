054c2caff2a975a215e61439ee0751ff099a30d1
Ege Eke <egeeke2001@gmail.com>
2022-04-06 02:06:58 +0300

    🐛 Fix `AssetListViewLayout` no content layout

--- Classes/Assets/Addition/List/Layout/AssetListViewLayout.swift
+++ Classes/Assets/Addition/List/Layout/AssetListViewLayout.swift
@@ -27,6 +27,8 @@ final class AssetListViewLayout: NSObject {
 
     private let listDataSource: AssetListViewDataSource
 
+    private let sectionHorizontalInsets: LayoutHorizontalPaddings = (24, 24)
+
     init(
         listDataSource: AssetListViewDataSource
     ) {
@@ -62,6 +64,31 @@ extension AssetListViewLayout: UICollectionViewDelegateFlowLayout {
         }
     }
 
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        insetForSectionAt section: Int
+    ) -> UIEdgeInsets {
+        let sectionIdentifiers = listDataSource.snapshot().sectionIdentifiers
+
+        guard let listSection = sectionIdentifiers[safe: section] else {
+            return .zero
+        }
+        
+        var insets =
+        UIEdgeInsets(
+            (0, sectionHorizontalInsets.leading, 0, sectionHorizontalInsets.trailing)
+        )
+
+        switch listSection {
+        case .empty:
+            return insets
+        case .assets:
+            insets.top = 24
+            return insets
+        }
+    }
+
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         handlers.didSelectAssetAt?(indexPath)
     }
@@ -94,7 +121,10 @@ extension AssetListViewLayout {
     private func calculateContentWidth(
         for listView: UICollectionView
     ) -> LayoutMetric {
-        return listView.bounds.width - listView.contentInset.horizontal
+        return listView.bounds.width -
+        listView.contentInset.horizontal -
+        sectionHorizontalInsets.leading -
+        sectionHorizontalInsets.trailing
     }
 
     private func sizeForSearchNoContent(
