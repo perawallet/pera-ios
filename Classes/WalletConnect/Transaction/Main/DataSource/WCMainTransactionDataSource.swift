4e58162db7d9e5cc94e3290d85028734f123f3a3
Ömer Aslan <omer@hipo.biz>
2022-07-28 13:59:52 +0300

    Revert "🐛 Fix grouping indexes issue when any transaction signed by dApp"
    This reverts commit 4359892f6f35ef8d701270af14fb30def11eaaff.

--- Classes/WalletConnect/Transaction/Main/DataSource/WCMainTransactionDataSource.swift
+++ Classes/WalletConnect/Transaction/Main/DataSource/WCMainTransactionDataSource.swift
@@ -48,47 +48,28 @@ class WCMainTransactionDataSource: NSObject {
     }
 
     private func groupTransactions(_ transactions: [WCTransaction]) {
-        let groupIndexes = generateGroupIndexes(transactions)
+        let transactionData = transactions.compactMap { $0.unparsedTransactionDetail }
+        var error: NSError?
+        let verifiedGroups = AlgorandSDK().findAndVerifyTransactionGroups(for: transactionData, error: &error)
 
-        guard groupIndexes.count == transactions.count else {
+        if error != nil {
             delegate?.wcMainTransactionDataSourceDidFailedGroupingValidation(self)
             return
         }
 
-        for (index, group) in groupIndexes.enumerated() {
+        guard let groups = verifiedGroups,
+              groups.count == transactions.count else {
+            delegate?.wcMainTransactionDataSourceDidFailedGroupingValidation(self)
+            return
+        }
+
+        for (index, group) in groups.enumerated() {
             if groupedTransactions[group] == nil {
                 groupedTransactions[group] = [transactions[index]]
             } else {
                 groupedTransactions[group]?.append(transactions[index])
             }
         }
-    }
-
-    private func generateGroupIndexes(_ transactions: [WCTransaction]) -> [Int64] {
-        var indexes: [String: Int64] = [:]
-        var currentIndex: Int64 = 0
-        var groupIndexes: [Int64] = []
-
-        for transaction in transactions {
-            guard let groupId = transaction.transactionDetail?.transactionGroupId else {
-                groupIndexes.append(currentIndex)
-
-                currentIndex = currentIndex.advanced(by: 1)
-                continue
-            }
-
-            if let index = indexes[groupId] {
-                groupIndexes.append(index)
-                continue
-            }
-
-            indexes[groupId] = currentIndex
-            groupIndexes.append(currentIndex)
-
-            currentIndex = currentIndex.advanced(by: 1)
-        }
-
-        return groupIndexes
     }
 }
 
