6a90e01bbcfca9bbd891c477cc49ba0ae896d4fe
Ahmet C. Sahin <sahin.ahmetcemal@gmail.com>
2022-09-01 01:24:17 +0300

    ✨ Create `SwapDividerView` - Also, create `Segments` group for the view

---
+++ Classes/Swap/Main/Views/SwapDivider/SwapDividerView.swift
@@ -0,0 +1,72 @@
+// Copyright 2022 Pera Wallet, LDA
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//    http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//   SwapDividerView.swift
+
+import Foundation
+import MacaroonUIKit
+import UIKit
+
+final class SwapDividerView: View {
+    private lazy var switchControl = SegmentedControl()
+    private lazy var setAmountControl = SegmentedControl(distributionMode: .fillProportionally)
+
+    func customize(_ theme: SwapDividerViewTheme) {
+        addSeparator(theme)
+        addSwitch(theme)
+        addSetAmount(theme)
+    }
+
+    func customizeAppearance(_ styleSheet: NoStyleSheet) {}
+
+    func prepareLayout(_ layoutSheet: NoLayoutSheet) {}
+}
+
+extension SwapDividerView {
+    private func addSeparator(_ theme: SwapDividerViewTheme) {
+        addSeparator(theme.horizontalSeparator)
+    }
+
+    private func addSwitch(_ theme: SwapDividerViewTheme) {
+        switchControl.backgroundImage = theme.backgroundImage
+        switchControl.add(segment: SwitchSegment())
+
+        addSubview(switchControl)
+        switchControl.snp.makeConstraints {
+            $0.leading == theme.horizontalPadding
+            $0.top.bottom == 0
+        }
+    }
+
+    private func addSetAmount(_ theme: SwapDividerViewTheme) {
+        setAmountControl.separatorImage = theme.verticalSeparatorImage
+        setAmountControl.backgroundImage = theme.backgroundImage
+
+        var amountAdjustSegment = AmountAdjustSegment()
+        amountAdjustSegment.configureForDisabledState()
+        var maxSegment = MaxSegment()
+        maxSegment.configureForDisabledState()
+
+        setAmountControl.add(segments: [
+            amountAdjustSegment,
+            maxSegment
+        ])
+
+        addSubview(setAmountControl)
+        setAmountControl.snp.makeConstraints {
+            $0.trailing == theme.horizontalPadding
+            $0.top.bottom == 0
+        }
+    }
+}
---
+++ Classes/Swap/Main/Views/SwapDivider/SwapDividerViewTheme.swift
@@ -0,0 +1,37 @@
+// Copyright 2022 Pera Wallet, LDA
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//    http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//   SwapDividerViewTheme.swift
+
+import Foundation
+import MacaroonUIKit
+import UIKit
+
+struct SwapDividerViewTheme: StyleSheet, LayoutSheet {
+    let backgroundImage: UIImage
+    let horizontalSeparator: Separator
+    let horizontalPadding: LayoutMetric
+    let verticalSeparatorImage: UIImage
+
+    init(_ family: LayoutFamily) {
+        self.backgroundImage = "swap-divider-segment-bg".uiImage
+        self.horizontalSeparator = Separator(
+            color: Colors.Layer.grayLighter,
+            size: 1,
+            position: .centerY((0, 0))
+        )
+        self.horizontalPadding = 24
+        self.verticalSeparatorImage = "swap-divider-separator".uiImage
+    }
+}
