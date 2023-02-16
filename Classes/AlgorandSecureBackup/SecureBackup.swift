afa2dde4884ca5a2b70c8b476fb911b4f4cedf6b
Ömer Aslan <omer@hipo.biz>
2023-02-16 17:27:35 +0300

    ✨ Add backup models

---
+++ Classes/AlgorandSecureBackup/SecureBackup.swift
@@ -0,0 +1,37 @@
+// Copyright 2023 Pera Wallet, LDA
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//    http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//   SecureBackup.swift
+
+import Foundation
+
+final class SecureBackup: ALGAPIModel {
+    let version: String
+    let suite: String
+    let cipherText: Data
+
+    init() {
+        version = ""
+        suite = ""
+        cipherText = Data()
+    }
+}
+
+extension SecureBackup {
+    enum CodingKeys: String, CodingKey {
+        case version
+        case suite
+        case cipherText = "ciphertext"
+    }
+}
