77beaaf4aea8ccffa99f7b4f792d595ec9dc2d2b
Ömer Aslan <omer@hipo.biz>
2023-02-28 16:03:00 +0300

    🐛 Fix announcement filtering

--- Classes/Demo/HomeAPIDataController.swift
+++ Classes/Demo/HomeAPIDataController.swift
@@ -262,9 +262,6 @@ extension HomeAPIDataController: AnnouncementAPIDataControllerDelegate {
         _ dataController: AnnouncementAPIDataController,
         didFetch announcements: [Announcement]
     ) {
-        let announcementToDisplay = announcements.first { announcement in
-            return !session.isAnnouncementHidden(announcement)
-        }
-        self.visibleAnnouncement = announcementToDisplay
+        self.visibleAnnouncement = announcements.first
     }
 }
