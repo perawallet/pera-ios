202ca7ebd912b87d06fb74f39f5262840f8fe3ab
Ömer Aslan <omer@hipo.biz>
2023-04-06 10:38:10 +0200

    ♻️ Refactor in app browser screen push dapp detail function
    - Moved duplicated codes to in app browser screen

--- Classes/Discover/Generic/DiscoverGenericScreen.swift
+++ Classes/Discover/Generic/DiscoverGenericScreen.swift
@@ -18,7 +18,7 @@ import Foundation
 import UIKit
 import WebKit
 
-final class DiscoverGenericScreen: DiscoverInAppBrowserScreen<DiscoverGenericScriptMessage> {
+final class DiscoverGenericScreen: DiscoverInAppBrowserScreen<NoInAppBrowserScriptMessage> {
     init(
         params: DiscoverGenericParameters,
         configuration: ViewControllerConfiguration
@@ -28,85 +28,4 @@ final class DiscoverGenericScreen: DiscoverInAppBrowserScreen<DiscoverGenericScr
             configuration: configuration
         )
     }
-
-    /// <mark>
-    /// WKScriptMessageHandler
-    override func userContentController(
-        _ userContentController: WKUserContentController,
-        didReceive message: WKScriptMessage
-    ) {
-        let inAppMessage = DiscoverGenericScriptMessage(rawValue: message.name)
-
-        switch inAppMessage {
-        case .none:
-            super.userContentController(
-                userContentController,
-                didReceive: message
-            )
-        case .pushDappViewerScreen:
-            handleDappDetailAction(message)
-        }
-    }
-}
-
-extension DiscoverGenericScreen {
-    private func isAcceptable(_ message: WKScriptMessage) -> Bool {
-        let frameInfo = message.frameInfo
-
-        if !frameInfo.isMainFrame { return false }
-        if frameInfo.request.url.unwrap(where: \.isPeraURL) == nil { return false }
-
-        return true
-    }
-
-    private func handleDappDetailAction(_ message: WKScriptMessage) {
-        if !isAcceptable(message) { return }
-
-        guard let jsonString = message.body as? String else { return }
-        guard let jsonData = jsonString.data(using: .utf8) else { return }
-        guard let params = try? DiscoverDappParamaters.decoded(jsonData) else { return }
-        navigateToDappDetail(params)
-    }
-
-    private func navigateToDappDetail(_ params: DiscoverDappParamaters) {
-        let screen: Screen = .discoverDappDetail(params) {
-            [weak self] event in
-            guard let self else { return }
-
-            switch event {
-            case .addToFavorites(let dappDetails):
-                self.addToFavorites(dappDetails)
-            case .removeFromFavorites(let dappDetails):
-                self.removeFromFavorites(dappDetails)
-            }
-        }
-
-        open(
-            screen,
-            by: .push
-        )
-    }
-
-    private func addToFavorites(_ dapp: DiscoverFavouriteDappDetails) {
-        updateFavorites(dapp: dapp)
-    }
-
-    private func removeFromFavorites(_ dapp: DiscoverFavouriteDappDetails) {
-        updateFavorites(dapp: dapp)
-    }
-
-    private func updateFavorites(dapp: DiscoverFavouriteDappDetails) {
-        guard let dappDetailsString = try? dapp.encodedString() else {
-            return
-        }
-
-        let scriptString = "var message = '" + dappDetailsString + "'; handleMessage(message);"
-        self.webView.evaluateJavaScript(scriptString)
-    }
-}
-
-enum DiscoverGenericScriptMessage:
-    String,
-    InAppBrowserScriptMessage {
-    case pushDappViewerScreen
 }
