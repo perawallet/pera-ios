aba1be97c75e081ced7ae2a2e961ccb8bd13247e
Ege Eke <egeeke2001@gmail.com>
2023-06-29 00:31:09 +0300

    🐛 Fix rekey support URL

--- Classes/Application/Environment/Environment.swift
+++ Classes/Application/Environment/Environment.swift
@@ -164,7 +164,7 @@ enum AlgorandWeb: String {
     case tinymanTermsOfService = "https://tinyman.org/terms-of-service"
     case tinymanSwapMain = "https://app.tinyman.org/#/swap?asset_in=0"
     case tinymanSwap = "http://perawallet.app/support/swap/"
-    case rekey = "https://perawallet.app/rekey"
+    case rekey = "https://perawallet.app/support/rekey/"
 
     var presentation: String {
         switch self {
