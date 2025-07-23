b2130b7d0580b17e832bb598c3b68ad775660986
Adrian Truszczyński <adrian.truszczynski@gmail.com>
2025-07-23 14:18:27 +0200

    Fixed unit tests

--- algorand-tests/Scenes/Settings List/SettingsListModelTests.swift
+++ algorand-tests/Scenes/Settings List/SettingsListModelTests.swift
@@ -33,107 +33,8 @@ struct SettingsListModelTests {
     
     @MainActor
     init() throws {
-        let session = try #require(AppDelegate.shared!.appConfiguration.session)
         accountsService = AccountServiceMock()
-        settingsListModel = SettingsListModel(accountsService: accountsService, appVersion: appVersion, legacySessionManager: session)
-    }
-    
-    // MARK: - Tests - Accounts that needs backup count
-    
-    @Test("Accounts that needs backup count for empty wallet")
-    func accountsNeedBackupCountForEmptyWallet() async throws {
-        
-        try await update(accounts: [])
-        
-        let accountsNeedBackupCountCollection = accountsNeedBackupCountCollection()
-        let accountsNeedBackupCount = try #require(accountsNeedBackupCountCollection.first)
-        
-        #expect(accountsNeedBackupCountCollection.count == 1)
-        #expect(accountsNeedBackupCount == 0)
-    }
-    
-    @Test("Accounts that needs backup count for non empty wallet without excluded accounts",
-          arguments: [
-            [
-                PeraAccount(address: "123", type: .algo25, authType: nil),
-                PeraAccount(address: "234", type: .algo25, authType: nil),
-                PeraAccount(address: "345", type: .algo25, authType: nil),
-                PeraAccount(address: "456", type: .algo25, authType: nil),
-                PeraAccount(address: "567", type: .algo25, authType: nil)
-            ],
-            [
-                PeraAccount(address: "123", type: .algo25, authType: .wallet),
-                PeraAccount(address: "234", type: .algo25, authType: .wallet),
-                PeraAccount(address: "345", type: .algo25, authType: .wallet),
-                PeraAccount(address: "456", type: .algo25, authType: .wallet),
-                PeraAccount(address: "567", type: .algo25, authType: .wallet)
-            ],
-            [
-                PeraAccount(address: "123", type: .algo25, authType: .ledger),
-                PeraAccount(address: "234", type: .algo25, authType: .ledger),
-                PeraAccount(address: "345", type: .algo25, authType: .ledger),
-                PeraAccount(address: "456", type: .algo25, authType: .ledger),
-                PeraAccount(address: "567", type: .algo25, authType: .ledger)
-            ],
-            [
-                PeraAccount(address: "123", type: .universalWallet, authType: nil),
-                PeraAccount(address: "234", type: .universalWallet, authType: nil),
-                PeraAccount(address: "345", type: .universalWallet, authType: nil),
-                PeraAccount(address: "456", type: .universalWallet, authType: nil),
-                PeraAccount(address: "567", type: .universalWallet, authType: nil)
-            ],
-            [
-                PeraAccount(address: "123", type: .watch, authType: nil),
-                PeraAccount(address: "234", type: .watch, authType: nil),
-                PeraAccount(address: "345", type: .watch, authType: nil),
-                PeraAccount(address: "456", type: .watch, authType: nil),
-                PeraAccount(address: "567", type: .watch, authType: nil)
-            ],
-            [
-                PeraAccount(address: "123", type: .algo25, authType: nil),
-                PeraAccount(address: "234", type: .universalWallet, authType: .wallet),
-                PeraAccount(address: "345", type: .watch, authType: .ledger),
-                PeraAccount(address: "456", type: .algo25, authType: nil),
-                PeraAccount(address: "567", type: .universalWallet, authType: nil)
-            ]
-          ]
-    )
-    func accountsNeedBackupCountForNonEmptyWallet(accounts: [PeraAccount]) async throws {
-        
-        try await update(accounts: accounts)
-        
-        let accountsNeedBackupCountCollection = accountsNeedBackupCountCollection()
-        let accountsNeedBackupCount = try #require(accountsNeedBackupCountCollection.first)
-        
-        #expect(accountsNeedBackupCountCollection.count == 1)
-        #expect(accountsNeedBackupCount == 5)
-    }
-    
-    @Test("Accounts need backup count for non empty wallet with excluded accounts",
-          arguments: [
-            [PeraAccount(address: "001", type: .invalid, authType: nil), PeraAccount(address: "002", type: .invalid, authType: nil)],
-            [PeraAccount(address: "001", type: .watch, authType: .invalid), PeraAccount(address: "002", type: .watch, authType: .invalid)],
-            [PeraAccount(address: "001", type: .invalid, authType: nil), PeraAccount(address: "002", type: .universalWallet, authType: .invalid)]
-          ]
-    )
-    func accountsNeedBackupCountForNonEmptyWalletWithExcludedAccounts(excludedAccounts: [PeraAccount]) async throws {
-        
-        var accounts = [
-            PeraAccount(address: "123", type: .algo25, authType: .wallet),
-            PeraAccount(address: "234", type: .algo25, authType: .wallet),
-            PeraAccount(address: "345", type: .algo25, authType: .wallet),
-            PeraAccount(address: "456", type: .algo25, authType: .wallet),
-            PeraAccount(address: "567", type: .algo25, authType: .wallet)
-        ]
-        
-        accounts += excludedAccounts
-        try await update(accounts: accounts)
-        
-        let accountsNeedBackupCountCollection = accountsNeedBackupCountCollection()
-        let accountsNeedBackupCount = try #require(accountsNeedBackupCountCollection.first)
-        
-        #expect(accountsNeedBackupCountCollection.count == 1)
-        #expect(accountsNeedBackupCount == 5)
+        settingsListModel = SettingsListModel(appVersion: appVersion)
     }
     
     // MARK: - Tests - App Version
@@ -149,19 +50,6 @@ struct SettingsListModelTests {
     private func update(accounts: [PeraAccount]) async throws {
         accountsService.accountsPublisher.send(accounts)
         try await Task.sleep(for: .milliseconds(10.0))
-    }
-    
-    private func accountsNeedBackupCountCollection() -> [Int] {
-        settingsListModel.viewModel.sections
-            .flatMap(\.rows)
-            .compactMap {
-                switch $0 {
-                case let .secureBackup(accountsNeedBackupCount):
-                    return accountsNeedBackupCount
-                default:
-                    return nil
-                }
-            }
     }
 }
 
