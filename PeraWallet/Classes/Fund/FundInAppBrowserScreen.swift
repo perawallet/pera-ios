0a518f32b64708c4efc791b02a4efe4f02f11f5d
Filipe Mota <1306746+filipetamota@users.noreply.github.com>
2026-01-20 15:28:06 +0000

    load webview when view is loaded instead of when view appears (#250)

--- PeraWallet/Classes/Fund/FundInAppBrowserScreen.swift
+++ PeraWallet/Classes/Fund/FundInAppBrowserScreen.swift
@@ -46,10 +46,14 @@ class FundInAppBrowserScreen: InAppBrowserScreen {
     override func customizeTabBarAppearence() {
         tabBarHidden = false
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadPeraURL()
+    }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        loadPeraURL()
         DispatchQueue.main.asyncAfter(wallDeadline: .now() + 1.0) { [weak self] in
             guard let self else { return }
             self.updateTheme(self.traitCollection.userInterfaceStyle)
