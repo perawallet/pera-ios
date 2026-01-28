db51115a8ab0f010a300ea3057c7fc2d2a666792
Filipe Mota <1306746+filipetamota@users.noreply.github.com>
2026-01-28 08:59:41 +0000

    [iOS] Account List not formatted Correctly (#257)
    * fetch accounts name endpoint implemented
    
    * nf domain saved and presented in subtitle
    
    * small fix

---
+++ PeraWalletCore/Models/Accounts/Account/Response/Account+Name.swift
@@ -0,0 +1,82 @@
+// Copyright 2022-2026 Pera Wallet, LDA
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//    http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//   Account+Name.swift
+
+public final class AccountName: ALGEntityModel {
+    public let results: [AccountNameResult]
+
+    public init(
+        _ apiModel: APIModel = APIModel()
+    ) {
+        self.results = apiModel.results
+    }
+
+    public func encode() -> APIModel {
+        var apiModel = APIModel()
+        apiModel.results = results
+        return apiModel
+    }
+}
+
+extension AccountName {
+    public struct APIModel: ALGAPIModel {
+        var results: [AccountNameResult]
+
+        public init() {
+            self.results = []
+        }
+
+        private enum CodingKeys: String, CodingKey {
+            case results
+        }
+    }
+}
+
+public final class AccountNameResult: ALGAPIModel {
+    public let address: String
+    public let nameResult: NameResult?
+
+    public init() {
+        self.address = .empty
+        self.nameResult = nil
+    }
+    
+    private enum CodingKeys:
+        String,
+        CodingKey {
+        case address
+        case nameResult = "name"
+    }
+}
+
+public final class NameResult: ALGAPIModel {
+    public let name: String?
+    public let source: String?
+    public let image: String?
+
+    public init() {
+        self.name = nil
+        self.source = nil
+        self.image = nil
+    }
+    
+    private enum CodingKeys:
+        String,
+        CodingKey {
+        case name
+        case source
+        case image
+    }
+}
